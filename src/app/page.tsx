201fa180e0ce9dcfd7f2fca8c1d7e1d974b1cedf
Plouzeau Alexandre <plouzeau.alexandre@hotmail.fr>
2024-10-15 13:32:02 +0200

    fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { Header, Footer, Buttons } from "./components";
 import Link from "next/link";
 
 export default function Home() {
--- src/app/pages/present/page.tsx
+++ src/app/pages/present/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function present() {
   return <p>Je m'appelle Ludivine</p>;
 }
