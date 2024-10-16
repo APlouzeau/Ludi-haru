ffd14540b8d467f5b3289a8ae342dff6443cfb62
Plouzeau Alexandre <plouzeau.alexandre@hotmail.fr>
2024-10-16 23:31:17 +0200

    BDD ok

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className=" flex justify-center content-center w-100 bg-gradient-to-r from-blue-500 to-cyan-500 min-h-24">
       <p className="">Copyright</p>
-      <Link href="/pages/contactme" className="">
+      <Link href="/contactme" className="">
         Me contacter
       </Link>
     </footer>
--- src/app/lessons/data.tsx
+++ src/app/lessons/data.tsx
@@ -2,22 +2,22 @@ export const lessonsData = [
   {
     id: 1,
     title: "Cours de Français enfant",
-    description: "Les enfants apprenent le Français",
     date: "01-01-2024",
+    description: "Les enfants apprennent le Français",
     author: "Ludivine Plouzeau",
   },
   {
     id: 2,
     title: "Cours de Français adulte",
-    description: "Les adultes apprenent le Français",
     date: "01-02-2024",
+    description: "Les adultes apprennent le Français",
     author: "Ludivine Plouzeau",
   },
   {
     id: 3,
     title: "Cours de Français avancé",
-    description: "Les gens perfectionnent le Français",
     date: "01-03-2024",
+    description: "Les gens perfectionnent le Français",
     author: "Ludivine Plouzeau",
   },
 ];
