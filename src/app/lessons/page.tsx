e0c29e41b9ccc9dfe59742c6ffce123fb7c1b21a
Plouzeau Alexandre <plouzeau.alexandre@hotmail.fr>
2024-10-17 11:53:47 +0200

    test deploy 1

--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -36,32 +36,39 @@ export default function Lessons() {
   console.log(lessons);
 
   return (
-    <div className=" flex flex-wrap justify-around">
-      {lessons.map((lesson) => {
-        const link = `/lessons/${lesson.id}`;
-        return (
-          <div
-            key={lesson.id.toString()}
-            className="card card-compact bg-base-100 w-96 shadow-xl m-4"
-          >
-            <figure>
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{lesson.title}</h2>
-              <p>{lesson.description}</p>
-              <div className="card-actions justify-end">
-                <Link href={link}>
-                  <button className="btn btn-primary">En savoir plus</button>
-                </Link>
+    <div>
+      <div className=" flex flex-wrap justify-around">
+        {lessons.map((lesson) => {
+          const link = `/lessons/${lesson.id}`;
+          return (
+            <div
+              key={lesson.id.toString()}
+              className="card card-compact bg-base-100 w-96 shadow-xl m-4"
+            >
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{lesson.title}</h2>
+                <p>{lesson.description}</p>
+                <div className="card-actions justify-end">
+                  <Link href={link}>
+                    <button className="btn btn-primary">En savoir plus</button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className="text-center m-2">
+        <Link href="/newlesson">
+          <button className="btn btn-primary">Ajouter une leçon</button>
+        </Link>
+      </div>
     </div>
   );
 }
