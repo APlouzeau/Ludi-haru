cfb18e8c63cfb68657974930942876f4c599d3d7
Plouzeau Alexandre <plouzeau.alexandre@hotmail.fr>
2024-10-18 21:22:53 +0200

    fix error syntax nextjs

--- src/app/present/page.tsx
+++ src/app/present/page.tsx
@@ -1,3 +1,3 @@
 export default function present() {
-  return <p>Je m'appelle Ludivine et je suis professeure de français</p>;
+  return <p>Je m&apos;appelle Ludivine et je suis professeure de français</p>;
 }
